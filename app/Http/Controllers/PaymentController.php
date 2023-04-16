cf8cd32955097f0713b0c91e9c782288a44e9080
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2023-04-16 08:56:08 +0600

    cashIn or cashOut done

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+
+class PaymentController extends Controller
+{
+    // cashIn payment add 
+    public function cashInPayment(Request $request){
+
+        // validation
+        $request->validate([
+            'name'              => 'required',
+            'amount'            => 'required',
+            'payment_recevier'  => 'required',
+            'payment_type'      => 'required',
+            'description'       => 'required'
+        ]);
+
+        // date or time
+        $time = date("g:i a");   
+        $date= date("Y-m-d");   
+
+
+        // data store to DB
+        Payment::create([
+            'name'              => $request->name,
+            'amount'            => $request->amount,
+            'payment_recevier'  => $request->payment_recevier,
+            'payment_type'      => $request->payment_type,
+            'description'       => $request->description,
+            'date'              => $date,
+            'time'              => $time,
+            'status'            => 'CashIn'
+        ]);
+
+        // response to api
+        return response()->json([
+            'status'    => 200,
+            'message'   => 'CashIn received successfully',
+        ]);
+    }
+
+    // cashOut payment add 
+    public function cashOutPayment(Request $request){
+
+        // validation
+        $request->validate([
+            'name'              => 'required',
+            'amount'            => 'required',
+            'payment_recevier'  => 'required',
+            'payment_type'      => 'required',
+            'description'       => 'required'
+        ]);
+
+        // date or time
+        $time = date("g:i a");   
+        $date= date("Y-m-d");   
+
+
+        // data store to DB
+        Payment::create([
+            'name'              => $request->name,
+            'amount'            => $request->amount,
+            'payment_recevier'  => $request->payment_recevier,
+            'payment_type'      => $request->payment_type,
+            'description'       => $request->description,
+            'date'              => $date,
+            'time'              => $time,
+            'status'            => 'CashOut'
+        ]);
+
+        // response to api
+        return response()->json([
+            'status'    => 200,
+            'message'   => 'CashOut received successfully',
+        ]);
+    }
+
+
+    // all payment list show
+    public function PaymentListShow(){
+
+        // get all payment data
+        $payment_list = Payment::all();
+
+        // return response
+        return response()->json([
+            'status'        => 200,
+            'message'       => 'All payment list',
+            'data'          => $payment_list
+        ]);
+    }
+}
